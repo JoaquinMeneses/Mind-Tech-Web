798faf7aa32c3a629526d19d0557b1e7eb7be6b6
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-06-09 13:27:18 -0300

    Zustand: global states

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -5,16 +5,44 @@ import Index from "../pages/Index";
 import Login from "../pages/login";
 import Store from "../pages/Store";
 
+import useStore from "../store/store";
+
 export default function RouterApp() {
+  const {
+    allProducts,
+    getAllProducts,
+    oneProduct,
+    getOneProduct,
+    user,
+    getUser,
+    token,
+    login,
+    logout
+  } = useStore();
+
   return (
     <Routes>
       <Route element={<App />}>
         <Route path="/" element={<Index />} />
-       
       </Route>
-      <Route path="/register/login" element= {<Login/>} />
-      <Route path="/demostration" element={<Demostration />} />
-      <Route path="/store" element= {<Store/>} />
+      <Route path="/register/login" element={<Login />} />
+      <Route
+        path="/demostration"
+        element={
+          <Demostration
+            allProducts={allProducts}
+            getAllProducts={getAllProducts}
+            oneProduct={oneProduct}
+            getOneProduct={getOneProduct}
+            user={user}
+            getUser={getUser}
+            token={token}
+            login={login}
+            logout={logout}
+          />
+        }
+      />
+      <Route path="/store" element={<Store />} />
     </Routes>
   );
 }
